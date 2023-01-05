8c347a36dce971c2166a47fcb148de2c7487b0ea
ines luna <inesluna19@gmail.com>
2023-01-05 10:03:43 +0100

    añadido router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import store from './reducer/store';
-import { Provider } from 'react-redux';
+
 import { GeneralView } from './pages/GeneralView';
 import { NavBar } from './components/NavBar';
 
 function App() {
 
     return (
-        <Provider store={store}>
+        <div>
             <NavBar />
             <GeneralView />
-        </Provider>
+        </div>
     );
 }
 
--- src/pages/GeneralView.jsx
+++ src/pages/GeneralView.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Provider, useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { increment } from '../reducer/counterSlice';
 import { getOompas } from '../bff/getOompas';
 import { useState, useEffect } from 'react';
 import { OompaGeneralCard } from '../components/OompaGeneralCard';
 import { SearchInput } from '../components/SearchInput';
+
 
 export const GeneralView = () => {
     const dispatch = useDispatch()
@@ -26,26 +27,26 @@ export const GeneralView = () => {
         console.log(oompasList, 'wthf')
     }, [oompasList]);
 
-  return (
-    <div className='md:mx-11'>
-        <SearchInput/>
-        <h1 className='text-5xl text-center mt-20'>Find your Oompa Loompa</h1>
-        <h3 className='text-4xl text-center text-slate-500 mb-20 mt-3'>There are more than 100k</h3>
-        {
-            oompasList.length > 0 ? (
-                <div className='flex flex-wrap p-3'>
-                    {
-                        oompasList.map((o, i) => {
-                            return(
-                                <OompaGeneralCard oompaInfo={o} key={o.id} />
-                            )
-                        })
-                    }
-                </div>
-            ) : (
-                <p>loading...</p>
-            )
-        }
-    </div>
-  );
+    return (
+        <div className='md:mx-11'>
+            <SearchInput/>
+            <h1 className='text-5xl text-center mt-20'>Find your Oompa Loompa</h1>
+            <h3 className='text-4xl text-center text-slate-500 mb-20 mt-3'>There are more than 100k</h3>
+            {
+                oompasList.length > 0 ? (
+                    <div className='flex flex-wrap p-3'>
+                        {
+                            oompasList.map((o, i) => {
+                                return(
+                                    <OompaGeneralCard oompaInfo={o} key={o.id} />
+                                )
+                            })
+                        }
+                    </div>
+                ) : (
+                    <p>loading...</p>
+                )
+            }
+        </div>
+    );
 };
